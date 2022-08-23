6de9760436816a913ee512d376818f7c9f629262
全能的饺子 <923537663@qq.com>
2022-08-23 09:58:42 +0800

    style(user): user style change

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,6 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/typescript/recommended',
-    '@vue/prettier',
-    '@vue/prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   parserOptions: {
