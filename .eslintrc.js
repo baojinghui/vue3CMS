c740ee4460b72ed490a403cf4fb63ddc9af03a73
全能的饺子 <923537663@qq.com>
2022-08-23 09:37:22 +0800

    01

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,20 @@ module.exports = {
   env: {
     node: true,
   },
+  //这里配置的为项目中代码要符合的规范，从第一个继承到最后一个，如果冲突最后一个会覆盖
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
-};
+}
